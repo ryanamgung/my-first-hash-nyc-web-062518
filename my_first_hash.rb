4fd40d311e2323ce548889ba6492de8c7e8784d5
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-19 02:00:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,7 +10,9 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   the_manifest = {"whale bone corsets" => 5,
                   "porcelain vases" => 2,
-                  "oil paintings" => 3}
+                  "oil paintings" => 3
+    
+  }
 
 
 end
@@ -23,7 +25,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+  shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -33,13 +35,15 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+  shipping_manifest["muskets" => 2]
 
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest["gun powder" => 4]
 
   # return the shipping_manifest hash below
+  
+  shipping_manifest
 
 end
